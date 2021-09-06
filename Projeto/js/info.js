820e0c3c392338ae88374710f30241f1e9f3b5f7
Ronaldo2017 <Ronaldo2017@users.noreply.github.com>
2021-09-06 08:38:17 -0300

    Projeto realizado no bootcamp da Digital Innovation One

---
+++ Projeto/js/info.js
@@ -0,0 +1,14 @@
+function voltar(){
+    window.history.back()
+}
+
+
+function informacao(){
+    window.location.href = "Projeto/Paginas_Infos/info.html"
+    'Peter Parker (Tom Holland) está em uma viagem de duas semanas pela Europa, ao lado de seus amigos de escola, quando é surpreendido pela visita de Nick Fury (Samuel L. ... Até que Nick o procura para resolver um problema: Homem-Aranha precisa combater os monstros Elementais, que estão destruindo tudo pela frente.'
+}
+
+function assistir(){
+    window.location.href = "Projeto/Paginas_Infos/assistir.html"
+}
+
